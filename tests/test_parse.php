9d3d176c866f2e7cebc4eeaa3bdb9187ad645fdd
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-05-19 18:23:54 +0200

    element parsing works

--- tests/test_parse.php
+++ tests/test_parse.php
@@ -8,12 +8,25 @@ class ParserTest extends PHPUnit_Framework_TestCase
 {
     function test_download_file()
     {
-        $content = download_file('http://www.google.com');
-        $this->assertContains('google.com', $content);
+        $content = download_file('out.html');
+        $this->assertContains('Deutsche Bahn', $content);
+        return $content;
     }
+
+
+    /**
+     * @depends test_download_file
+     */
+    function test_to_listing($content)
+    {
+        $listing = make_listing($content);
+        print_r($listing);
+    }
+
 
     public function test_foo()
     {
+        return;
         $fp = fopen('http://mobile.bahn.de/bin/mobil/bhftafel.exe/dox?si=106907&bt=arr&ti=13:39&p=1111101&max=20&rt=1&use_realtime_filter=1&date=19.05.11&start=yes', 'r');
         $content = stream_get_contents($fp);
 

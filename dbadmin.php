ac660a14857daa99dd95679dece8908d12205369
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-07-03 18:16:12 +0200

    beginning of the db stuff

---
+++ dbadmin.php
@@ -0,0 +1,33 @@
+<?php
+require('dbtools.php');
+global $db;
+
+function create_db() {
+    global $db;
+    $db->exec("create table fahrplan (
+        station,
+        target,
+        planed_arrival DATETIME,
+        drift_arrival DATETIME,
+        train,
+        line,
+        drift,
+    PRIMARY KEY(station, target, planed_arrival, train, line)
+    );") or die(print_r($db->errorInfo(), true));
+}
+
+function clear_db()
+{
+    $db->exec("drop from fahrplan where 1");
+}
+
+assert (PHP_SAPI == 'cli');
+
+
+$cmds = array_slice($argv, 1);
+foreach($cmds as $cmd) {
+    call_user_func("{$cmd}_db");
+}
+
+
+?>

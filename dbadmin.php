1c456deb6bc027e990177ccd790554167f77d113
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2011-07-04 23:59:58 +0200

    showdb + my_station global

--- dbadmin.php
+++ dbadmin.php
@@ -1,6 +1,7 @@
 <?php
 require('dbtools.php');
 require 'downloader.php';
+require 'table_printers.php';
 global $db;
 
 function create_db() {
@@ -34,17 +35,22 @@ function kill_db()
 function sync_db()
 {
     global $db;
-
-    $station = "Berlin";
+    global $my_station;
 
     $time = new DateTime();
-    $url = make_url($station, $time);
+    $url = make_url($my_station, $time);
     $content = file_get_contents($url);
     $listing = make_listing($content, $time);
     foreach($listing as &$item)
-        insert_or_update($station, $item);
+        insert_or_update($my_station, $item);
 }
 
+function show_db()
+{
+    global $db;
+    $items = get_current();
+    print draw_text_table($items);
+}
 assert (PHP_SAPI == 'cli');
 
 
